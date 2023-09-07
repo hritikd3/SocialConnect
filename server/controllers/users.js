2a5d34cd42cbf75eab7c18a9a47e27d2b1da863b
hritikd3 <hritikdangi3@gmail.com>
2023-09-07 10:44:39 +0530

    added the basic routes

---
+++ server/controllers/users.js
@@ -0,0 +1,81 @@
+import { format } from "morgan";
+import User from "../models/User";
+
+// READ
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({
+      message: err.message,
+    });
+  }
+};
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return {
+          _id,
+          firstName,
+          lastName,
+          occupation,
+          location,
+          picturePath,
+        };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// UPDATE
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((friend) => friend !== friendId);
+      friend.friends = friend.friends.filter((uId) => uId !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((uId) => User.findById(uId))
+    );
+
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return {
+          _id,
+          firstName,
+          lastName,
+          occupation,
+          location,
+          picturePath,
+        };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  };
+};
--- server/index.js
+++ server/index.js
@@ -7,13 +7,12 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import  path  from "path";
+import path from "path";
 import { fileURLToPath } from "url";
-import {register} from "./controllers/auth.js"
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { verifyToken } from "./middleware/auth.js";
-
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -43,21 +42,22 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILES
-app.post("/auth/register", upload.single("picture"), 
-register);
+app.post("/auth/register", upload.single("picture"), register);
 
 // Routes
-app.use("/auth", authRoutes)
-app.use("/users", userRoutes)
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE SETUP
-const PORT= process.env.PORT ||  6001; //BACK UP PORT 6001
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:  true,
+const PORT = process.env.PORT || 6001; //BACK UP PORT 6001
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(()=>{
-    app.listen(PORT, ()=>{ console.log(`Server Port: ${PORT}`)});
-
-})
-.catch((error)=> console.log(`${error} did not connect`))
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
