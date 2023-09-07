53bb3b7367b6dbd65db2f0c630168319fc02002c
hritikd3 <hritikdangi3@gmail.com>
2023-09-07 16:43:10 +0530

    routes defined

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import {createPost} from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATION */
@@ -43,10 +45,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts",verifyToken, upload.single("picture"), createPost);
 
 // Routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001; //BACK UP PORT 6001
