530d9c41835893e9196e0cc8d59aade144f933d9
hritikd3 <hritikdangi3@gmail.com>
2023-10-10 23:24:32 +0530

    init client

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import {createPost} from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js"
+import Post from "./models/Posts.js"
+import {users,posts} from "./data/index.js"
+
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -63,5 +67,10 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server Port: ${PORT}`);
     });
+
+
+    // // add data one time 
+    // User.insertMany(users)
+    // Post.insertMany(posts)
   })
   .catch((error) => console.log(`${error} did not connect`));
