a753fe570aa6d653a9a208907facd0f39f05f1ee
hritikd3 <hritikdangi3@gmail.com>
2023-10-11 10:45:32 +0530

    routes set for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom' ;
+
+import HomePage from './pages/homePage';
+import LoginPage from './pages/loginPage';
+import ProfilePage from './pages/profilePage';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<LoginPage />} />
+    <Route path='/home' element={<HomePage />} />
+    <Route path='/profile/:userId' element={<ProfilePage />} />
+   </Routes>
+   </BrowserRouter>
     </div>
   );
 }
