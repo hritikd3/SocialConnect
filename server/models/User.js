1ea0185d485b549d0d04414497dd23ec3291cb86
hritikd3 <hritikdangi3@gmail.com>
2023-11-20 00:16:21 +0530

    schema for user

--- server/models/User.js
+++ server/models/User.js
@@ -1,48 +1,46 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 5,
+      unique: true,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impression: Number,
   },
-  lastName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    max: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 5,
-    unique: true,
-  },
-  picturePath: {
-    type: String,
-    default: "",
-  },
-  friends: {
-    type: Array,
-    default: []
-  },
-  location: String,
-  occupation: String,
-  viewedProfile: Number,
-  impression: Number,
-},
-{ timestamps: true}
-
-
+  { timestamps: true }
 );
 
-
-const User= mongoose.model("User" , UserSchema)
+const User = mongoose.model("User", UserSchema);
 export default User;
