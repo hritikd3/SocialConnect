0b83a14c4935e5757abec4b46ab3a78a91657e44
hritikd3 <hritikdangi3@gmail.com>
2023-11-20 00:14:49 +0530

    initial

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -12,40 +12,36 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     setMode: (state) => {
-      //here the first state.mode refers to the previous condition line no 4(:light)
-
       state.mode = state.mode === "light" ? "dark" : "light";
     },
     setLogin: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
     },
-    setLogout:(state)=>{
-      state.user= null;
-      state.token= null;
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
     },
-    setFriends:(state, action)=>{
-      if(state.user){
-        state.user.friends= action.payload.friends;
-      }else{
-        console.log("user friends not exist")
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent :(");
       }
     },
-    setPosts:(state, action)=>{
-      state.posts= action.payload.posts;
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
     },
-    setPost: (state, action)=>{
- const updatedPosts= state.posts.map((post)=>{
-  if(post._id=== action.payload.post_id)
-  return action.payload.post.post_id;
-
-  return post;
- })
- state.posts= updatedPosts;
-    }
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
 
-
-export const{ setMode, setLogin, setFriends, setLogout, setPost, setPosts}= authSlice.actions;
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
 export default authSlice.reducer;
