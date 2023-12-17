32912e2b37ad9627c8cb68d528e3ae9b66a10059
hritikd3 <hritikdangi3@gmail.com>
2023-12-17 11:20:17 +0530

    modify

--- client/src/pages/widgets/MyPostWidgets.jsx
+++ client/src/pages/widgets/MyPostWidgets.jsx
@@ -24,20 +24,18 @@ import WidgetWrapper from "../../components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../state";
+const MyPostWidgets = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
 
-const MyPostWidgets= ({picturePath})=>{
-    const dispatch= useDispatch();
-    const [isImage, setIsImage]= useState(false);
-    const [image, setImage]= useState(null);
-    const [post, setPost]= useState("");
-    const {palette}= useTheme();
-    const {_id}= useSelector((state)=> state.user)
-    const token= useSelector((state)=> state.token)
-    const isNonMobileScreens= useMediaQuery("(min-wdith: 1000px)")
-    const mediumMain= palette.neutral.mediumMain;
-    const medium= palette.neutral.medium;
-
- 
   const handlePost = async () => {
     const formData = new FormData();
     formData.append("userId", _id);
