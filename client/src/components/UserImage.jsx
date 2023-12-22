a95506691ecfcda1be3c38ace6279180c9d2aed5
hritikd3 <hritikdangi3@gmail.com>
2023-12-22 21:01:00 +0530

    added blue border across the user icon

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,16 +1,23 @@
-import { Box } from "@mui/material";
 
-const UserImage = ({ image, size = "60px" }) => {
+import { Avatar } from "@mui/material";
+import {styled} from "@mui/system";
+
+const StyledAvatar = styled(Avatar)({
+  width: "100%",
+  height: "100%",
+  borderRadius: "50%",
+  border: "2px solid #00D5FA", // Added border
+  // borderColor: (theme) => theme.palette.primary.main, // Added blue border color
+});
+
+const UserImage = ({ image, size = "50px", onClick }) => {
   return (
-    <Box width={size} height={size}>
-      <img
-        style={{ objectFit: "cover", borderRadius: "50%" }}
-        width={size}
-        height={size}
-        alt="user"
-        src={`http://localhost:3001/assets/${image}`}
-      />
-    </Box>
+    <StyledAvatar
+      alt="user"
+      src={`http://localhost:3001/assets/${image}`}
+      sx={{ width: size, height: size, cursor: onClick ? "pointer" : "default" }}
+      onClick={onClick}
+    />
   );
 };
 
